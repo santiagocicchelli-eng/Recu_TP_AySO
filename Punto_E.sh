8c84ba7acae35d185736442a699c2647f20ee5c2
santiagocicchelli-eng <santiagocicchelli@gmail.com>
2026-05-28 01:35:30 +0000

    ADD: Punto_E.sh

--- Punto_E.sh
+++ Punto_E.sh
@@ -1,7 +1,14 @@
-#!/bin/bash
+#Comandos Mi ip Publica: 
+ curl -s ifconfig.me
 
-echo "Info de Mi CPU"
-lscpu | grep -iE "model name|MHz"
+echo "Info de Ip Publica" 
+Mi IP Publica es: 181.1.49.143
 
-echo "Info de Memoria Ram"
-free -h
+#########################################
+#Comandos CPU Model:
+ lscpu | grep -iE "model name|MHz"
+
+echo "Info Procesador"
+CPU Modelo: AMD Ryzen 5 7520U with Radeon Graphics
+
+
