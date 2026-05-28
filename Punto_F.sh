a686fce8bd3f8a55fe0a1c2a01c5c42735015993
santiagocicchelli-eng <santiagocicchelli@gmail.com>
2026-05-28 01:31:33 +0000

    ADD: Punto_F.sh

--- Punto_F.sh
+++ Punto_F.sh
@@ -1,5 +1,16 @@
-#CPU Model:
- lscpu | grep -iE "model name|MHz"
+#!/bin/bash
 
-#Mi ip Publica es: 
- curl -s ifconfig.me
+echo "Comando Info de Mi CPU"
+lscpu | grep -iE "model name|MHz"
+
+#Cpu Modelo
+Model name: AMD Ryzen 5 7520U with Radeon Graphics
+
+#######################################################################################
+
+echo "Comando Info de Memoria Ram"
+Free -h
+
+#Memoria Ram
+total        used        free      shared  buff/cache   available
+Mem:        1.9Gi       177Mi       1.4Gi       0.0Ki       314Mi       1.6Gi
